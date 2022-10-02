bb260f42cc068a15eeeac0ff10d1bf9852afbebc
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-01 20:34:46 -0400

    Don't need extra arg

--- slothpu_interface.py
+++ slothpu_interface.py
@@ -9,9 +9,14 @@ def top_handler(key):
 
 class PipelineStage(urwid.WidgetWrap):
     def __init__(self, target: SlothPU):
-        pipeline_txt = urwid.Text(target.pipeline_stage, align=urwid.LEFT, wrap=urwid.CLIP)
-        box = urwid.LineBox(pipeline_txt, title="Current Pipeline Stage:", title_align=urwid.LEFT)
+        self._target = target
+        self._pipeline_txt = urwid.Text(self._target.pipeline_stage, align=urwid.LEFT, wrap=urwid.CLIP)
+        box = urwid.LineBox(self._pipeline_txt, title="Current Pipeline Stage:", title_align=urwid.LEFT)
         super(PipelineStage, self).__init__(urwid.Filler(box, valign=urwid.MIDDLE))
+
+    def advance_pipeline(self):
+        self._target.advance_pipeline()
+        self._pipeline_txt.set_text(self._target.pipeline_stage)
 
 
 class SlothPU_Interface:
@@ -26,6 +31,9 @@ class SlothPU_Interface:
     def input_filter(self, input, raw_input):
         if 'q' in input or 'Q' in input:
             raise urwid.ExitMainLoop()
+
+        if ' ' in input:
+            self.top.advance_pipeline(  )
 
         # Prevent further processing of input
         return []
