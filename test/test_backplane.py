06afe7b3b848a9150d1d090efadc718468372038
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-02 11:52:23 -0400

    Smoke test for the bus

---
+++ test/test_backplane.py
@@ -0,0 +1,9 @@
+from slothpu import BackPlane
+
+def test_smoke():
+    num_bits = 8
+    target = BackPlane(num_bits)
+
+    assert target.A_bus.n_bits == num_bits
+    assert target.B_bus.n_bits == num_bits
+    assert target.W_bus.n_bits == num_bits
