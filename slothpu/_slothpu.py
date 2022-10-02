ffd7dbb7d0bce6eb4119775fee247d4b054ca5bd
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-01 20:57:20 -0400

    Adding the registers

--- slothpu/_slothpu.py
+++ slothpu/_slothpu.py
@@ -1,3 +1,6 @@
+import bitarray
+import bitarray.util
+
 pipeline_stages = [
     "Fetch0",
     "Fetch1",
@@ -9,10 +12,15 @@ pipeline_stages = [
 ]
 n_pipeline_stages = len(pipeline_stages)
 
+n_bits_per_byte = 8
+n_registers = 8
 
 class SlothPU:
     def __init__(self):
         self._pipeline_stage: int = n_pipeline_stages - 1
+        self._registers = [bitarray.util.zeros(n_bits_per_byte) for _ in range(n_registers)]
+        for i in range(n_registers):
+            self._registers[i][i] = 1
 
     @property
     def pipeline_stage(self) -> str:
@@ -25,3 +33,7 @@ class SlothPU:
             self._pipeline_stage = 0
         else:
             self._pipeline_stage = (self._pipeline_stage + 1) % (n_pipeline_stages - 1)
+
+    def get_register(self, i :int)->bitarray.bitarray:
+        assert len(self._registers[i]) == n_bits_per_byte
+        return self._registers[i]
