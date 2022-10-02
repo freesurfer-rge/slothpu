6b8fb2718833fd515f752b4a226e66ba0b890364
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-02 09:43:49 -0400

    blacken

--- slothpu/_register_file.py
+++ slothpu/_register_file.py
@@ -10,13 +10,14 @@ class RegisterFile:
         # for bit 0 to be the least significant
         self._n_bits = n_bits
         self._registers = [
-            bitarray.util.zeros(self.n_bits, endian="little") for _ in range(n_registers)
+            bitarray.util.zeros(self.n_bits, endian="little")
+            for _ in range(n_registers)
         ]
         for i in range(n_registers):
             self._registers[i][i] = 1
 
     @property
-    def n_bits(self)->int:
+    def n_bits(self) -> int:
         return self._n_bits
 
     def __len__(self):
@@ -25,23 +26,23 @@ class RegisterFile:
     def __getitem__(self, key: int) -> bitarray.bitarray:
         assert isinstance(self._registers[key], bitarray.bitarray)
         assert len(self._registers[key]) == self.n_bits
-        assert self._registers[key].endian() == 'little'
+        assert self._registers[key].endian() == "little"
         return self._registers[key]
 
-    def __setitem__(self, key: int, value: Union[str,bitarray.bitarray]):
+    def __setitem__(self, key: int, value: Union[str, bitarray.bitarray]):
         assert isinstance(value, str) or isinstance(value, bitarray.bitarray)
         assert key >= 0
         assert key < len(self)
 
         if isinstance(value, bitarray.bitarray):
-            assert value.endian() == 'little'
+            assert value.endian() == "little"
             assert len(value) <= self.n_bits
             # Make sure we have a copy
             self._registers[key] = bitarray.bitarray(value)
         else:
             # Assume that a string is supplied big-endian
             # This means we must reverse it before supplying to the constructor
-            self._registers[key] = bitarray.bitarray(value[::-1], endian='little')
+            self._registers[key] = bitarray.bitarray(value[::-1], endian="little")
 
-        self._registers[key].extend('0' * (self.n_bits-len(self._registers[key])))
+        self._registers[key].extend("0" * (self.n_bits - len(self._registers[key])))
         assert len(self._registers[key]) == self.n_bits
