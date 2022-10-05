5456f8a87f07ff5ff06c61f18356de907a410b00
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-04 21:15:05 -0400

    Another command

--- slothpu/_program_counter.py
+++ slothpu/_program_counter.py
@@ -22,4 +22,16 @@ class ProgramCounter:
         self._pc, _ = bitarray_add(self.pc, step, carry_in=0)
 
     def execute(self, command: str):
-        pass
+        if command == "INC":
+            self.increment()
+        elif command == "FETCH0":
+            # Put current address onto A_bus
+            self._backplane.A_bus.value = self._pc
+        elif command == "FETCH1":
+            # Put current address+1 onto A_bus
+            one = bitarray.util.int2ba(
+                1, length=self._backplane.n_bits, endian="little"
+            )
+            self._backplane.A_bus.value = self._pc | one
+        else:
+            raise ValueError("Unrecognised PC command: " + command)
--- test/test_program_counter.py
+++ test/test_program_counter.py
@@ -17,3 +17,37 @@ def test_increment():
     assert bitarray.util.ba2int(target.pc) == 0
     target.increment()
     assert bitarray.util.ba2int(target.pc) == 2
+
+
+def test_execute_increment():
+    bp = BackPlane(8)
+    target = ProgramCounter(bp)
+
+    assert bitarray.util.ba2int(target.pc) == 0
+    target.execute("INC")
+    target.execute("INC")
+    assert bitarray.util.ba2int(target.pc) == 4
+
+
+def test_execute_fetch0():
+    bp = BackPlane(8)
+    target = ProgramCounter(bp)
+
+    target.increment()
+    target.increment()
+    assert bitarray.util.ba2int(target.pc) == 4
+    assert bitarray.util.ba2int(bp.A_bus.value) == 0
+    target.execute("FETCH0")
+    assert bitarray.util.ba2int(bp.A_bus.value) == 4
+
+
+def test_execute_fetch1():
+    bp = BackPlane(8)
+    target = ProgramCounter(bp)
+
+    target.increment()
+    target.increment()
+    assert bitarray.util.ba2int(target.pc) == 4
+    assert bitarray.util.ba2int(bp.A_bus.value) == 0
+    target.execute("FETCH1")
+    assert bitarray.util.ba2int(bp.A_bus.value) == 5
