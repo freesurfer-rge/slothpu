2f51705033315e44f070652e7a74fe5a86598d6d
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-04 21:31:17 -0400

    blacken

--- test/test_program_counter.py
+++ test/test_program_counter.py
@@ -52,6 +52,7 @@ def test_execute_fetch1():
     target.execute("FETCH1")
     assert bitarray.util.ba2int(bp.A_bus.value) == 5
 
+
 def test_execute_branch():
     bp = BackPlane(8)
     target = ProgramCounter(bp)
@@ -60,29 +61,10 @@ def test_execute_branch():
     target.increment()
     assert bitarray.util.ba2int(target.pc) == 4
     loc = 10
-    bp.A_bus.value = bitarray.util.int2ba(loc, length=bp.n_bits, endian='little')
+    bp.A_bus.value = bitarray.util.int2ba(loc, length=bp.n_bits, endian="little")
     target.execute("BRANCH")
     assert bitarray.util.ba2int(target.pc) == loc
 
-def test_execute_branch_if_zero():
-    bp = BackPlane(8)
-    target = ProgramCounter(bp)
-
-    target.increment()
-    target.increment()
-    assert bitarray.util.ba2int(target.pc) == 4
-    loc = 130
-    bp.A_bus.value = bitarray.util.int2ba(loc, length=bp.n_bits, endian='little')
-
-    # Set up B_bus to be non-zero
-    bp.B_bus.value = bitarray.util.int2ba(2, length=bp.n_bits, endian='little')
-    target.execute("BRANCH_IF_ZERO")
-    assert bitarray.util.ba2int(target.pc) == 6 # We will have incremented instead
-
-    # Now have B_bus be zero
-    bp.B_bus.value = bitarray.util.zeros(8, endian='little')
-    target.execute("BRANCH_IF_ZERO")
-    assert bitarray.util.ba2int(target.pc) == loc
 
 def test_execute_branch_if_zero():
     bp = BackPlane(8)
@@ -92,14 +74,35 @@ def test_execute_branch_if_zero():
     target.increment()
     assert bitarray.util.ba2int(target.pc) == 4
     loc = 130
-    bp.A_bus.value = bitarray.util.int2ba(loc, length=bp.n_bits, endian='little')
+    bp.A_bus.value = bitarray.util.int2ba(loc, length=bp.n_bits, endian="little")
 
     # Set up B_bus to be non-zero
-    bp.B_bus.value = bitarray.util.int2ba(2, length=bp.n_bits, endian='little')
+    bp.B_bus.value = bitarray.util.int2ba(2, length=bp.n_bits, endian="little")
+    target.execute("BRANCH_IF_ZERO")
+    assert bitarray.util.ba2int(target.pc) == 6  # We will have incremented instead
+
+    # Now have B_bus be zero
+    bp.B_bus.value = bitarray.util.zeros(8, endian="little")
+    target.execute("BRANCH_IF_ZERO")
+    assert bitarray.util.ba2int(target.pc) == loc
+
+
+def test_execute_branch_if_zero():
+    bp = BackPlane(8)
+    target = ProgramCounter(bp)
+
+    target.increment()
+    target.increment()
+    assert bitarray.util.ba2int(target.pc) == 4
+    loc = 130
+    bp.A_bus.value = bitarray.util.int2ba(loc, length=bp.n_bits, endian="little")
+
+    # Set up B_bus to be non-zero
+    bp.B_bus.value = bitarray.util.int2ba(2, length=bp.n_bits, endian="little")
     target.execute("BRANCH_IF_NONZERO")
     assert bitarray.util.ba2int(target.pc) == loc
 
     # Now have B_bus be zero
-    bp.B_bus.value = bitarray.util.zeros(8, endian='little')
+    bp.B_bus.value = bitarray.util.zeros(8, endian="little")
     target.execute("BRANCH_IF_NONZERO")
-    assert bitarray.util.ba2int(target.pc) == loc + 2 # Increment instead
+    assert bitarray.util.ba2int(target.pc) == loc + 2  # Increment instead
