2819810dbfa290f0c9fc8c0c1242336f9ae78264
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-05 19:25:11 -0400

    Enabling 14 bit addressing

--- test/test_main_memory.py
+++ test/test_main_memory.py
@@ -7,22 +7,23 @@ def test_smoke():
     n_bits = 8
     bp = BackPlane(n_bits)
 
-    target = MainMemory(2**n_bits, bp)
+    target = MainMemory(bp)
 
-    target_loc = 127
-    tl_ba = bitarray.util.int2ba(target_loc, endian="little")
-    tl_ba.fill()  # Since n_bits=8
-    bp.A_bus.value = tl_ba
-    assert bp.W_bus.value == bitarray.util.zeros(8, endian="little")
+    target_loc = 4000
+    assert target_loc < 2**14 # Only have 14 bits of address lines
+    tl_ba = bitarray.util.int2ba(target_loc, 16, endian="little")
+    bp.A_bus.value = tl_ba[0:8]
+    bp.B_bus.value = tl_ba[8:16]
+    assert bp.C_bus.value == bitarray.util.zeros(8, endian="little")
     target.execute("READ")
     # Memory values are initialised to their location
-    assert bitarray.util.ba2int(bp.W_bus.value) == target_loc
+    assert bitarray.util.ba2int(bp.C_bus.value) == target_loc %  2**n_bits
 
     # Now write
     write_value = 21
-    bp.B_bus.value = bitarray.util.int2ba(write_value, length=n_bits, endian="little")
+    bp.C_bus.value = bitarray.util.int2ba(write_value, length=n_bits, endian="little")
     target.execute("WRITE")
     # Read it back
-    assert bitarray.util.ba2int(bp.W_bus.value) == target_loc  # Should be unchanged
+    bp.C_bus.value = bitarray.util.zeros(8, endian='little')
     target.execute("READ")
-    assert bitarray.util.ba2int(bp.W_bus.value) == write_value
+    assert bitarray.util.ba2int(bp.C_bus.value) == write_value
