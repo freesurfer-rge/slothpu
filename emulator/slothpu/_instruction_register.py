36274ca8ea8224d52701f7e2b4286e766dffac50
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-06 20:03:22 -0400

    Working on the IR

--- emulator/slothpu/_instruction_register.py
+++ emulator/slothpu/_instruction_register.py
@@ -9,6 +9,10 @@ class InstructionRegister:
         self._bp = backplane
         self._n_bits = 2 * self._bp.n_bits
         self._ir = bitarray.util.zeros(self.n_bits, endian="little")
+        self._unit = 0
+        self._R_A = 0
+        self._R_B = 0
+        self._R_C = 0
 
     @property
     def ir(self):
@@ -20,6 +24,22 @@ class InstructionRegister:
     def n_bits(self) -> int:
         return self._n_bits
 
+    @property
+    def unit(self) -> int:
+        return self._unit
+
+    @property
+    def R_A(self) -> int:
+        return self._R_A
+
+
+    @property
+    def R_B(self) -> int:
+        return self._R_B
+
+    @property
+    def R_C(self) -> int:
+        return self._R_C
     
     def get_as_string(self) -> str:
         return to_01_bigendian(self.ir)
--- emulator/slothpu/interface/_instruction_register_widget.py
+++ emulator/slothpu/interface/_instruction_register_widget.py
@@ -8,9 +8,15 @@ class InstructionRegisterWidget(urwid.WidgetWrap):
         self._ir = ir
 
         self._ir_text = urwid.Text(self._ir.get_as_string())
+        self._unit_text = urwid.Text(f"Unit: {self._ir.unit}")
+        self._R_A_text = urwid.Text(f"R_A : {self._ir.R_A}")
+        self._R_B_text = urwid.Text(f"R_B : {self._ir.R_B}")
+        self._R_C_text = urwid.Text(f"R_C : {self._ir.R_C}")
+
+        ir_pile = urwid.Pile([self._ir_text, self._unit_text, self._R_A_text, self._R_B_text, self._R_C_text])
 
         ir_box = urwid.LineBox(
-            self._ir_text, title="Instruction Register", title_align=urwid.LEFT
+            ir_pile, title="Instruction Register", title_align=urwid.LEFT
         )
 
         super(InstructionRegisterWidget, self).__init__(ir_box)
