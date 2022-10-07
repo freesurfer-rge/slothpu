d148b10953fa1f37a37d11957bf50584d0575126
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-07 11:12:03 -0400

    Adding an increment_enable flag

--- emulator/slothpu/interface/_program_counter_widget.py
+++ emulator/slothpu/interface/_program_counter_widget.py
@@ -7,13 +7,19 @@ class ProgramCounterWidget(urwid.WidgetWrap):
     def __init__(self, pc: ProgramCounter):
         self._pc = pc
 
-        self._pc_text = urwid.Text(self._pc.get_as_string())
+        self._pc_text = urwid.Text("")
+        self._inc_enable_text = urwid.Text("")
+
+        pc_pile = urwid.Pile([self._pc_text, self._inc_enable_text])
 
         pc_box = urwid.LineBox(
-            self._pc_text, title="Program Counter", title_align=urwid.LEFT
+            
+            pc_pile, title="Program Counter", title_align=urwid.LEFT
         )
+        self.update()
 
         super(ProgramCounterWidget, self).__init__(pc_box)
 
     def update(self):
         self._pc_text.set_text(self._pc.get_as_string())
+        self._inc_enable_text.set_text(f"Increment Enable: {self._pc.increment_enable}")
