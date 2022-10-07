78620f6228f6e600a8e84ffa56a77d62cfb56175
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-07 16:11:33 -0400

    Handling various flake8 things

--- emulator/slothpu/__init__.py
+++ emulator/slothpu/__init__.py
@@ -1,9 +1,9 @@
 # Import all the bits & pieces
 
-from ._backplane import BackPlane
-from ._bus import Bus
-from ._instruction_register import InstructionRegister
-from ._main_memory import MainMemory
-from ._memory import Memory
-from ._program_counter import ProgramCounter
-from ._slothpu import SlothPU
+from ._backplane import BackPlane  # noqa: F401
+from ._bus import Bus  # noqa: F401
+from ._instruction_register import InstructionRegister  # noqa: F401
+from ._main_memory import MainMemory  # noqa: F401
+from ._memory import Memory  # noqa: F401
+from ._program_counter import ProgramCounter  # noqa: F401
+from ._slothpu import SlothPU  # noqa: F401
