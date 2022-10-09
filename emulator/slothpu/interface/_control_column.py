34b8975df34f3fabbb2115ec4e5ebe1f84e7ac72
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-08 21:48:39 -0400

    Add a widget

--- emulator/slothpu/interface/_control_column.py
+++ emulator/slothpu/interface/_control_column.py
@@ -4,6 +4,7 @@ from slothpu import SlothPU
 
 from ._instruction_register_widget import InstructionRegisterWidget
 from ._program_counter_widget import ProgramCounterWidget
+from ._status_register_widget import StatusRegisterWidget
 
 
 class ControlColumn(urwid.WidgetWrap):
@@ -12,15 +13,17 @@ class ControlColumn(urwid.WidgetWrap):
 
         pc_widget = ProgramCounterWidget(self._spu.program_counter)
         ir_widget = InstructionRegisterWidget(self._spu.instruction_register)
+        sr_widget = StatusRegisterWidget(self._spu.status_register)
 
         control_pile = urwid.Pile(
             [
                 urwid.Filler(pc_widget, valign=urwid.TOP),
                 urwid.Filler(ir_widget, valign=urwid.TOP),
+                urwid.Filler(sr_widget, valign=urwid.TOP)
             ]
         )
 
-        self._widgets = [pc_widget, ir_widget]
+        self._widgets = [pc_widget, ir_widget, sr_widget]
 
         super(ControlColumn, self).__init__(control_pile)
 
