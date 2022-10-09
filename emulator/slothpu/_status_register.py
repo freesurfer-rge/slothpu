fa2a7a8003362707f018285d5d6c6bdae15cd138
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-08 20:55:25 -0400

    Start thinking about the status register

---
+++ emulator/slothpu/_status_register.py
@@ -0,0 +1,34 @@
+import bitarray
+import bitarray.util
+
+from ._backplane import BackPlane
+
+from ._utils import to_01_bigendian
+
+class StatusRegister:
+    salu_bit = 0
+    dalu_bit = 1
+
+    def __init__(self,  backplane: BackPlane):
+        self._backplane = backplane
+        self._n_bits = self._backplane.n_bits
+        self._value = bitarray.util.zeros(self.n_bits, endian='little')
+
+    @property
+    def n_bits(self) -> int:
+        return self._n_bits
+
+    @property
+    def value(self) -> bitarray.bitarray:
+        assert len(self._value)==self.n_bits
+        assert self._value.endian() == 'little'
+        return self._value
+
+    def update(self):
+        assert len(self._value)==self.n_bits
+        assert self._value.endian() == 'little'
+        self._value[StatusRegister.salu_bit] == self._backplane.SALU_flag
+        self._value[StatusRegister.dalu_bit] == self._backplane.DALU_flag
+
+    def get_as_string(self):
+        return to_01_bigendian(self._value)
