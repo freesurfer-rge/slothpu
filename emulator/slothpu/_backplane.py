f3e3eee0e05b9593445c2eb9e8742d86323b1b02
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-08 20:45:01 -0400

    Reinstate flags in backplane

--- emulator/slothpu/_backplane.py
+++ emulator/slothpu/_backplane.py
@@ -25,3 +25,25 @@ class BackPlane:
     @property
     def C_bus(self) -> Bus:
         return self._C_bus
+        
+    @property
+    def SALU_flag(self) -> int:
+        assert self._salu_flag == 0 or self._salu_flag == 1
+        return self._salu_flag
+
+    @SALU_flag.setter
+    def SALU_flag(self, value: int):
+        assert isinstance(value, int)
+        assert value == 0 or value == 1
+        self._salu_flag = value
+
+    @property
+    def DALU_flag(self) -> int:
+        assert self._dalu_flag == 0 or self._dalu_flag == 1
+        return self._dalu_flag
+
+    @DALU_flag.setter
+    def DALU_flag(self, value: int):
+        assert isinstance(value, int)
+        assert value == 0 or value == 1
+        self._dalu_flag = value
