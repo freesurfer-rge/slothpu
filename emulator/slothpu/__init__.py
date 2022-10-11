1cda93998d1157f8c9ed4cc178f6dbffc57acc43
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-10 21:25:51 -0400

    Starting on the DALU

--- emulator/slothpu/__init__.py
+++ emulator/slothpu/__init__.py
@@ -2,6 +2,7 @@
 
 from ._backplane import BackPlane  # noqa: F401
 from ._bus import Bus  # noqa: F401
+from ._dalu import DALU  # noqa: F401
 from ._instruction_register import InstructionRegister  # noqa: F401
 from ._main_memory import MainMemory  # noqa: F401
 from ._memory import Memory  # noqa: F401
---
+++ emulator/test/test_dalu.py
@@ -0,0 +1,24 @@
+import bitarray.util
+
+import pytest
+
+from slothpu import DALU, BackPlane
+
+@pytest.mark.parametrize(['a', 'b'], [(0,0), (1,0), (0, 1),(128, 128), (255, 1), (255, 255)])
+def test_add(a, b):
+    bp = BackPlane(8)
+    target = DALU(bp)
+
+    bp.A_bus.value = bitarray.util.int2ba(a, 8, endian="little")
+    bp.B_bus.value = bitarray.util.int2ba(b, 8, endian="little")
+    assert bitarray.util.ba2int(bp.C_bus.value) == 0
+    assert bp.DALU_flag == 0
+
+    target.execute("ADD")
+
+    c = a + b
+    assert c < 512, "Check inputs!"
+    expected_carry, expected_c = divmod(c, 256)
+
+    assert bitarray.util.ba2int(bp.C_bus.value) == expected_c
+    assert bp.DALU_flag == expected_carry
