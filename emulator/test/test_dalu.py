5fd32f2996c9c1d91e1a460acb2ff941e713eda9
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-10 21:39:04 -0400

    And XOR

--- emulator/test/test_dalu.py
+++ emulator/test/test_dalu.py
@@ -5,19 +5,25 @@ import pytest
 from slothpu import DALU, BackPlane
 
 
+a_b_pairs = [
+    (0, 0),
+    (1, 0),
+    (0, 1),
+    (128, 128),
+    (255, 1),
+    (1, 255),
+    (137, 2),
+    (101, 13),
+    (7, 240),
+    (8, 240),
+    (240, 8),
+    (255, 255),
+]
+
+
 @pytest.mark.parametrize(
     ["a", "b"],
-    [
-        (0, 0),
-        (1, 0),
-        (0, 1),
-        (128, 128),
-        (255, 1),
-        (1, 255),
-        (137, 2),
-        (7, 240),
-        (255, 255),
-    ],
+    a_b_pairs,
 )
 def test_add(a, b):
     bp = BackPlane(8)
@@ -40,17 +46,7 @@ def test_add(a, b):
 
 @pytest.mark.parametrize(
     ["a", "b"],
-    [
-        (0, 0),
-        (1, 0),
-        (0, 1),
-        (128, 128),
-        (255, 1),
-        (1, 255),
-        (137, 2),
-        (7, 240),
-        (255, 255),
-    ],
+    a_b_pairs,
 )
 def test_sub(a, b):
     bp = BackPlane(8)
@@ -71,19 +67,10 @@ def test_sub(a, b):
         assert bitarray.util.ba2int(bp.C_bus.value) == c + 256
         assert bp.DALU_flag == 1
 
+
 @pytest.mark.parametrize(
     ["a", "b"],
-    [
-        (0, 0),
-        (1, 0),
-        (0, 1),
-        (128, 128),
-        (255, 1),
-        (1, 255),
-        (137, 2),
-        (7, 240),
-        (255, 255),
-    ],
+    a_b_pairs,
 )
 def test_or(a, b):
     bp = BackPlane(8)
@@ -100,20 +87,8 @@ def test_or(a, b):
     assert bitarray.util.ba2int(bp.C_bus.value) == c
     assert bp.DALU_flag == 0
 
-@pytest.mark.parametrize(
-    ["a", "b"],
-    [
-        (0, 0),
-        (1, 0),
-        (0, 1),
-        (128, 128),
-        (255, 1),
-        (1, 255),
-        (137, 2),
-        (7, 240),
-        (255, 255),
-    ],
-)
+
+@pytest.mark.parametrize(["a", "b"], a_b_pairs)
 def test_and(a, b):
     bp = BackPlane(8)
     target = DALU(bp)
@@ -126,5 +101,22 @@ def test_and(a, b):
     target.execute("AND")
 
     c = a & b
+    assert bitarray.util.ba2int(bp.C_bus.value) == c
+    assert bp.DALU_flag == 0
+
+
+@pytest.mark.parametrize(["a", "b"], a_b_pairs)
+def test_xor(a, b):
+    bp = BackPlane(8)
+    target = DALU(bp)
+
+    bp.A_bus.value = bitarray.util.int2ba(a, 8, endian="little")
+    bp.B_bus.value = bitarray.util.int2ba(b, 8, endian="little")
+    assert bitarray.util.ba2int(bp.C_bus.value) == 0
+    assert bp.DALU_flag == 0
+
+    target.execute("XOR")
+
+    c = a ^ b
     assert bitarray.util.ba2int(bp.C_bus.value) == c
     assert bp.DALU_flag == 0
