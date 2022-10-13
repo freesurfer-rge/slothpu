283fb76d4f56a47ddef1a99e076e4778f89bfd1f
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-13 19:16:11 -0400

    Thinking a little about how jump register simplifies things

--- emulator/slothpu/_register_file.py
+++ emulator/slothpu/_register_file.py
@@ -7,7 +7,6 @@ class RegisterFile:
         self._backplane = backplane
         self._n_bits = self._backplane.n_bits
         self._registers = Memory(n_registers, self.n_bits)
-        self._write_B_register = False
         self._write_C_register = False
         self._A_register = 0
         self._B_register = 0
@@ -51,13 +50,6 @@ class RegisterFile:
         assert idx >= 0 and idx < len(self._registers)
         self._C_register = idx
 
-    @property
-    def write_B_register(self) -> bool:
-        return self._write_B_register
-
-    @write_B_register.setter
-    def write_B_register(self, value: bool) -> bool:
-        self._write_B_register = value
 
     @property
     def write_C_register(self) -> bool:
@@ -67,17 +59,13 @@ class RegisterFile:
     def write_C_register(self, value: bool) -> bool:
         self._write_C_register = value
 
-    def execute(self, command: str):
-        if command == "RegisterRead":
-            self._backplane.A_bus.value = self._registers[self.A_register]
-            if not self.write_B_register:
-                self._backplane.B_bus.value = self._registers[self.B_register]
-            if not self.write_C_register:
-                self._backplane.C_bus.value = self._registers[self.C_register]
-        elif command == "RegisterWrite":
-            if self.write_B_register:
-                self._registers[self.B_register] = self._backplane.B_bus.value
-            if self.write_C_register:
-                self._registers[self.C_register] = self._backplane.C_bus.value
-        else:
-            raise ValueError(f"RegisterFile unrecognised command : {command}")
+    def decode(self):
+        # Registers are placed on the backplane during the 'decode' step
+        self._backplane.A_bus.value = self._registers[self.A_register]
+        self._backplane.B_bus.value = self._registers[self.B_register]
+        if not self.write_C_register:
+            self._backplane.C_bus.value = self._registers[self.C_register]
+
+    def commit(self):
+        assert self._write_C_register is True
+        self._registers[self.C_register] = self._backplane.C_bus.value
