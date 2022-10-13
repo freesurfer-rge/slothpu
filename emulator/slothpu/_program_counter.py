75cc86b19aa8cf64eaf72d61a70e42d726fee0db
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-13 19:20:17 -0400

    Starting to add JR to PC

--- emulator/slothpu/_program_counter.py
+++ emulator/slothpu/_program_counter.py
@@ -11,6 +11,7 @@ class ProgramCounter:
         self._n_bits = 2 * self._backplane.n_bits
         self._increment_enable = True
         self._pc = bitarray.util.zeros(self.n_bits, endian="little")
+        self._jr = bitarray.util.zeros(self.n_bits, endian="little")
 
     @property
     def pc(self) -> bitarray.bitarray:
@@ -20,6 +21,13 @@ class ProgramCounter:
         return self._pc
 
     @property
+    def jr(self) -> bitarray.bitarray:
+        assert len(self._jr) == self.n_bits
+        assert self._jr.endian() == "little"
+        assert self._jr[0] == 0, "JR must be even"
+        return self._jr
+
+    @property
     def n_bits(self) -> int:
         return self._n_bits
 
@@ -27,8 +35,11 @@ class ProgramCounter:
     def increment_enable(self) -> bool:
         return self._increment_enable
 
-    def get_as_string(self) -> str:
+    def get_pc_as_string(self) -> str:
         return to_01_bigendian(self.pc)
+    
+    def get_jr_as_string(self) -> str:
+        return to_01_bigendian(self.jr)
 
     def _set_pc(self, value: bitarray.bitarray):
         assert isinstance(value, bitarray.bitarray)
@@ -37,6 +48,14 @@ class ProgramCounter:
         assert value[0] == 0, "Must set PC to be even"
         # Make sure we copy
         self._pc = bitarray.bitarray(value)
+
+    def _set_jr(self, value: bitarray.bitarray):
+        assert isinstance(value, bitarray.bitarray)
+        assert len(value) == self.n_bits
+        assert value.endian() == "little"
+        assert value[0] == 0, "Must set PC to be even"
+        # Make sure we copy
+        self.get_jr_as_string = bitarray.bitarray(value)
 
     def increment(self):
         step = bitarray.util.int2ba(2, length=self.n_bits, endian="little")
@@ -56,13 +75,14 @@ class ProgramCounter:
         self._backplane.B_bus.value = self._pc[8:16]
 
     def execute(self, command: str):
+        jump_address = self._backplane.A_bus.value + self._backplane.B_bus.value
         if command == "BRANCH":
             # Copy....
-            target = self._backplane.A_bus.value + self._backplane.B_bus.value
+            target = jump_address
             self._set_pc(target)
             self._increment_enable = False
         elif command == "BRANCH_IF_ZERO":
-            target = self._backplane.A_bus.value + self._backplane.B_bus.value
+            target = jump_address
             if bitarray.util.ba2int(self._backplane.C_bus.value) == 0:
                 self._set_pc(target)
                 self._increment_enable = False
