92597274e53a0737ee44367d0ea1432460ceb244
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-15 19:03:47 -0400

    Hook in assembler

--- emulator/run_slothpu.py
+++ emulator/run_slothpu.py
@@ -1,5 +1,31 @@
+import argparse
+
+from assembler import process_lines
+
 from slothpu.interface import SlothPU_Interface
 
+def build_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--assembler-file",
+        help="The file containing the assembly code",
+        required=True,
+    )
 
-spu = SlothPU_Interface()
-spu.main()
+    return parser
+
+
+def main():
+    parser = build_argparser()
+    args = parser.parse_args()
+
+    with open(args.assembler_file) as f_assembler:
+        raw_lines = f_assembler.readlines()
+    initial_memory = process_lines(raw_lines)
+
+    spu = SlothPU_Interface(initial_memory)
+    spu.main()
+
+
+if __name__ == "__main__":
+    main()
