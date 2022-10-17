6bc957ee814ad2428e47dbe90f27ba5741fedf31
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-16 20:59:51 -0400

    Button to advance a full instruction

--- emulator/slothpu/interface/_pipeline_stage_widget.py
+++ emulator/slothpu/interface/_pipeline_stage_widget.py
@@ -8,17 +8,23 @@ class PipelineStageWidget(urwid.WidgetWrap):
         self._spu: SlothPU = parent._target
         self._parent = parent
         self._curr_stage = urwid.Text(self._spu.pipeline_stage)
-        self._advance_button = urwid.Button("Advance Pipeline", on_press=self.on_click)
+        self._advance_pipeline_button = urwid.Button("Advance Pipeline", on_press=self.on_click_advance_stage)
+        self._advance_instruction_button = urwid.Button("Advance Instruction", on_press=self.on_click_advance_instruction)
 
         cs = urwid.Padding(self._curr_stage, align=urwid.LEFT)
-        ab = urwid.Padding(self._advance_button, align=urwid.RIGHT)
+        apb = urwid.Padding(self._advance_pipeline_button, align=urwid.RIGHT)
+        aib = urwid.Padding(self._advance_instruction_button, align=urwid.LEFT)
 
-        cols = urwid.Columns([cs, ab])
+        cols = urwid.Columns([cs, apb, aib])
 
         super(PipelineStageWidget, self).__init__(cols)
 
-    def on_click(self, choice):
+    def on_click_advance_stage(self, choice):
         self._spu.advance_pipeline()
+        self._parent.update()
+
+    def on_click_advance_instruction(self, choice):
+        self._spu.advance_instruction()
         self._parent.update()
 
     def update(self):
