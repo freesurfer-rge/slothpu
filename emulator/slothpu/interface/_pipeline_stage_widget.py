feb1e6e99e0910ae917c84545ba18837dd9b0409
Richard Edgar <freesurfer.rge@gmail.com>
2022-10-16 21:00:38 -0400

    blacken

--- emulator/slothpu/interface/_pipeline_stage_widget.py
+++ emulator/slothpu/interface/_pipeline_stage_widget.py
@@ -8,8 +8,12 @@ class PipelineStageWidget(urwid.WidgetWrap):
         self._spu: SlothPU = parent._target
         self._parent = parent
         self._curr_stage = urwid.Text(self._spu.pipeline_stage)
-        self._advance_pipeline_button = urwid.Button("Advance Pipeline", on_press=self.on_click_advance_stage)
-        self._advance_instruction_button = urwid.Button("Advance Instruction", on_press=self.on_click_advance_instruction)
+        self._advance_pipeline_button = urwid.Button(
+            "Advance Pipeline", on_press=self.on_click_advance_stage
+        )
+        self._advance_instruction_button = urwid.Button(
+            "Advance Instruction", on_press=self.on_click_advance_instruction
+        )
 
         cs = urwid.Padding(self._curr_stage, align=urwid.LEFT)
         apb = urwid.Padding(self._advance_pipeline_button, align=urwid.RIGHT)
