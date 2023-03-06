b201a473deba7651d0d42d9455c5a6d10b741073
R G Edgar <freesurfer.rge@gmail.com>
2023-03-05 21:08:25 -0500

    More general test

--- pcbs/python-testing/test_dalu_board.py
+++ pcbs/python-testing/test_dalu_board.py
@@ -35,6 +35,22 @@ input_decoder_rev1 = dict(
 instructions = instructions_rev1
 input_decoder = input_decoder_rev1
 
+# =======================================
+
+a_b_pairs = [
+    (0, 0),
+    (1, 0),
+    (0, 1),
+    (128, 128),
+    (255, 1),
+    (1, 255),
+    (137, 2),
+    (101, 13),
+    (7, 240),
+    (8, 240),
+    (240, 8),
+    (255, 255),
+]
 
 def result_from_input(input: List[bool]):
     assert len(input) == 30
@@ -131,3 +147,48 @@ class TestAND:
         acb.recv()
         assert acb.C() == A_val & B_val
         assert acb.ALU_Flag() == False
+
+    @pytest.mark.parametrize(["A", "B"], a_b_pairs)
+    def test_specific_values(self, A, B):
+        acb = ALUConnectorBoard()
+
+        expected_C = A & B
+        
+        # Initally, C should be zero since the select
+        # line will be high
+        acb.recv()
+        assert acb.C() == 0
+
+        # Now send the inputs, select the instruction
+        # enable the DALU and set to decode
+        acb.A(A)
+        acb.B(B)
+        acb.Instruction(instructions["AND"])
+        acb.Select(False)
+        acb.Phase("Decode")
+        acb.send()
+
+        # Retrieve the internal state
+        acb.recv()
+        inputs = acb.Inputs()
+        assert inputs[input_decoder["FLAG"]] == False
+        result = result_from_input(inputs)
+        assert result == expected_C
+
+        # Go to the execute phase
+        acb.Phase("Execute")
+        acb.send()
+
+        # Check that answer is now externally visible
+        acb.recv()
+        assert acb.C() == expected_C
+        assert not acb.ALU_Flag(), "Check ALU_flag cleared"
+
+        # Go to the commit phase
+        acb.Phase("Commit")
+        acb.send()
+
+        # Check that the answer is still available
+        acb.recv()
+        assert acb.C() == expected_C
+        assert not acb.ALU_Flag(), "Check ALU_flag cleared"
