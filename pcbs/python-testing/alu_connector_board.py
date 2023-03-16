c25ae6d181c70b994cea3d64f0bcb919f7ea3ad8
R Edgar <freesurfer.rge@gmail.com>
2023-03-16 19:19:42 -0400

    Python tests for DALU (#48)
    Adding tests for the DALU board which can run with `pytest`. Note that these are for the r1 board, which has the instruction decoder outputs connected backwards.

---
+++ pcbs/python-testing/alu_connector_board.py
@@ -0,0 +1,142 @@
+from typing import Dict, List, Union
+
+import bitarray.util
+
+from tester_board import TesterBoard
+
+
+class ALUConnectorBoard:
+    def __init__(self):
+        """Initialise with an internal TesterBoard."""
+        self._tb = TesterBoard()
+
+        # Enable all outputs
+        self._tb.enable_outputs([True for _ in range(5)])
+
+        # Set up the outputs; only have the Select line high
+        # (recall select is active-low)
+        self._outputs = [False for _ in range(self._tb.n_pins)]
+        self._outputs[self.Output_Pins["Select"]] = True
+        self.send()
+
+        # Read in the inputs
+        self.recv()
+
+    @property
+    def Output_Pins(self) -> Dict[str, Union[int, List[int]]]:
+        op = dict(
+            A_bus=[31, 29, 27, 25, 23, 21, 19, 17],
+            B_bus=[15, 13, 11, 9, 7, 5, 3, 1],
+            I=[33, 35, 37, 39],
+            Commit=32,
+            Execute=34,
+            Decode=36,
+            Select=38,
+        )
+        return op
+
+    @property
+    def Input_Pins(self) -> Dict[str, Union[int, List[int]]]:
+        ip = dict(
+            C_bus=[0, 1, 2, 3, 4, 5, 6, 7],
+            ALU_Flag=8,
+            In=[
+                19,
+                18,
+                17,
+                16,
+                15,
+                14,
+                12,
+                13,
+                10,
+                11,  # End of Set 0
+                29,
+                28,
+                27,
+                26,
+                25,
+                24,
+                22,
+                23,
+                20,
+                21,  # End of Set 1
+                39,
+                38,
+                37,
+                36,
+                35,
+                34,
+                32,
+                33,
+                30,
+                31,  # End of Set 2
+            ],
+        )
+        return ip
+
+    def recv(self):
+        self._inputs = self._tb.recv()
+
+    def send(self):
+        self._tb.send(self._outputs)
+
+    def _set_output_bus(self, value: int, bus_name: str):
+        assert isinstance(value, int)
+        assert value >= 0
+        assert value < 256
+
+        converted = bitarray.util.int2ba(value, length=8, endian="little")
+        for i in range(8):
+            self._outputs[self.Output_Pins[bus_name][i]] = converted[i]
+
+    def A(self, value: int):
+        self._set_output_bus(value, "A_bus")
+
+    def B(self, value: int):
+        self._set_output_bus(value, "B_bus")
+
+    def Select(self, value: bool):
+        self._outputs[self.Output_Pins["Select"]] = value
+
+    def Phase(self, phase: str):
+        pins = dict(Commit=False, Execute=False, Decode=False)
+        if phase == "Other":
+            pass
+        elif phase == "Decode":
+            pins["Decode"] = True
+        elif phase == "Execute":
+            pins["Execute"] = True
+        elif phase == "Commit":
+            pins["Commit"] = True
+        else:
+            raise ValueError(f"Bad phase: {phase}")
+
+        # Set the output pins
+        for k, v in pins.items():
+            self._outputs[self.Output_Pins[k]] = v
+
+    def Instruction(self, instructions: List[bool]):
+        assert len(instructions) == 4
+
+        for i in range(len(instructions)):
+            self._outputs[self.Output_Pins["I"][i]] = instructions[i]
+
+    def C(self) -> int:
+        C_pins = self.Input_Pins["C_bus"]
+        C_vals = []
+        for p in C_pins:
+            C_vals.append(self._inputs[p])
+
+        value = bitarray.util.ba2int(bitarray.bitarray(C_vals, endian="little"))
+        return value
+
+    def ALU_Flag(self) -> bool:
+        return self._inputs[self.Input_Pins["ALU_Flag"]]
+
+    def Inputs(self) -> List[int]:
+        result = []
+        for p in self.Input_Pins["In"]:
+            result.append(self._inputs[p])
+        assert len(result) == 30
+        return result
---
+++ pcbs/python-testing/test_dalu_board.py
@@ -0,0 +1,250 @@
+from typing import List
+
+import bitarray.util
+import pytest
+
+from alu_connector_board import ALUConnectorBoard
+
+# Rev1 board has decoder outputs backwards
+instructions_rev1 = dict(
+    ADD=[True, True, True, False],
+    SUB=[False, True, True, False],
+    OR=[True, True, False, False],
+    XOR=[False, True, False, False],
+    AND=[
+        True,
+        False,
+        False,
+        False,
+    ],
+    NAND=[False, False, False, False],
+)
+
+input_decoder_rev1 = dict(
+    ADD=29,
+    SUB=28,
+    OR=27,
+    XOR=26,
+    AND=25,
+    NAND=24,
+    FLAG=9,
+    RESULT=[1, 2, 3, 4, 5, 6, 7, 8],
+)
+
+instructions = instructions_rev1
+input_decoder = input_decoder_rev1
+
+# =======================================
+
+
+test_values = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    8,
+    16,
+    31,
+    32,
+    33,
+    64,
+    127,
+    128,
+    129,
+    250,
+    251,
+    252,
+    253,
+    254,
+    255,
+]
+
+
+def result_from_input(input_pins: List[bool]):
+    assert len(input_pins) == 30
+
+    result_bits = []
+    for p in input_decoder["RESULT"]:
+        result_bits.append(input_pins[p])
+
+    value = bitarray.util.ba2int(bitarray.bitarray(result_bits, endian="little"))
+    return value
+
+
+class TestDecoder:
+    @pytest.mark.parametrize(
+        "current_instruction", ["ADD", "SUB", "OR", "XOR", "AND", "NAND"]
+    )
+    def test_instruction_decode(self, current_instruction):
+        acb = ALUConnectorBoard()
+
+        # On initialisation, all selectors should be high
+        acb.recv()
+        inputs = acb.Inputs()
+        for k, v in input_decoder.items():
+            if k in instructions.keys():
+                assert inputs[v], f"Checking {k}"
+
+        # Now set an instruction
+        acb.Instruction(instructions[current_instruction])
+        acb.send()
+
+        # However, nothing should have changed yet
+        acb.recv()
+        inputs = acb.Inputs()
+        for k, v in input_decoder.items():
+            if k in instructions.keys():
+                assert inputs[v], f"Checking {k}"
+
+        # Now set select
+        acb.Select(False)
+        acb.send()
+
+        # We should be able to detect the instruction
+        acb.recv()
+        inputs = acb.Inputs()
+        for k, v in input_decoder.items():
+            if k in instructions.keys():
+                assert inputs[v] == (k != current_instruction), f"Checking {k}"
+
+        # And deselect again
+        acb.Select(True)
+        acb.send()
+
+        acb.recv()
+        inputs = acb.Inputs()
+        for k, v in input_decoder.items():
+            if k in instructions.keys():
+                assert inputs[v], f"Checking {k}"
+
+
+class TestOperations:
+    def compute_expected(self, A: int, B: int, operation: str):
+        assert A >= 0 and A < 256
+        assert B >= 0 and B < 256
+        flag = False
+
+        if operation == "AND":
+            result = A & B
+        elif operation == "NAND":
+            result = ~(A & B)
+        elif operation == "OR":
+            result = A | B
+        elif operation == "XOR":
+            result = A ^ B
+        elif operation == "ADD":
+            result = A + B
+            flag = result >= 256
+        elif operation == "SUB":
+            result = A - B
+            flag = result < 0
+        else:
+            raise ValueError(f"Unrecognised operation: {operation}")
+
+        if result < 0:
+            result = result + 256
+        if result >= 256:
+            result = result - 256
+
+        return result, flag
+
+    @pytest.mark.parametrize("operation", ["AND", "NAND", "OR", "XOR", "ADD", "SUB"])
+    def test_smoke(self, operation):
+        acb = ALUConnectorBoard()
+
+        A_val = 6
+        B_val = 130
+        C_expected, flag_expected = self.compute_expected(A_val, B_val, operation)
+
+        acb.A(A_val)
+        acb.B(B_val)
+        acb.Instruction(instructions[operation])
+        acb.Select(False)
+        acb.Phase("Decode")
+
+        acb.send()
+
+        acb.recv()
+        inputs = acb.Inputs()
+        assert inputs[input_decoder["FLAG"]] == flag_expected
+        result = result_from_input(inputs)
+        assert result == C_expected
+
+        acb.Phase("Execute")
+        acb.send()
+        acb.recv()
+        inputs = acb.Inputs()
+        assert inputs[input_decoder["FLAG"]] == flag_expected
+        result = result_from_input(inputs)
+        assert result == C_expected
+        assert acb.C() == C_expected
+        assert acb.ALU_Flag() == flag_expected
+
+        acb.Phase("Commit")
+        acb.send()
+        acb.recv()
+        assert acb.C() == C_expected
+        assert acb.ALU_Flag() == flag_expected
+
+        acb.Phase("Other")
+        acb.send()
+        acb.recv()
+        assert acb.C() == 0
+        assert acb.ALU_Flag() == flag_expected
+
+    @pytest.mark.parametrize("A", test_values)
+    @pytest.mark.parametrize("B", test_values)
+    @pytest.mark.parametrize("operation", ["AND", "NAND", "OR", "XOR", "ADD", "SUB"])
+    def test_specific_values(self, A, B, operation):
+        acb = ALUConnectorBoard()
+
+        expected_C, expected_flag = self.compute_expected(A, B, operation)
+
+        # Initally, C should be zero since the select
+        # line will be high
+        acb.recv()
+        assert acb.C() == 0
+
+        # Now send the inputs, select the instruction
+        # enable the DALU and set to decode
+        acb.A(A)
+        acb.B(B)
+        acb.Instruction(instructions[operation])
+        acb.Select(False)
+        acb.Phase("Decode")
+        acb.send()
+
+        # Retrieve the internal state
+        acb.recv()
+        inputs = acb.Inputs()
+        assert inputs[input_decoder["FLAG"]] == expected_flag
+        result = result_from_input(inputs)
+        assert result == expected_C
+
+        # Go to the execute phase
+        acb.Phase("Execute")
+        acb.send()
+
+        # Check that answer is now externally visible
+        acb.recv()
+        assert acb.C() == expected_C
+        assert acb.ALU_Flag() == expected_flag
+
+        # Go to the commit phase
+        acb.Phase("Commit")
+        acb.send()
+
+        # Check that the answer is still available
+        acb.recv()
+        assert acb.C() == expected_C
+        assert acb.ALU_Flag() == expected_flag
+
+        # Check that going to another phase puts
+        # the C bus into high impedance (which is
+        # pulled down on the Tester board itself)
+        acb.Phase("Other")
+        acb.send()
+        acb.recv()
+        assert acb.C() == 0
+        assert acb.ALU_Flag() == expected_flag
